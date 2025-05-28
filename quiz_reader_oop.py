6af6eee86f7f7aea99335dd00606b1f977110322
unknown <sainttrowagmail.com>
2025-05-28 11:25:45 +0800

    Imported the parts in my quiz reader that plays the quiz file and also tweaked it abit, and also fixed some indentations

--- quiz_reader_oop.py
+++ quiz_reader_oop.py
@@ -37,17 +37,17 @@ class QuizReader: #Imported the get quiz file for it probably works the same in 
         for line in file_lines:
             line = line.strip() 
 
-        if line.startswith("Question"): #To check if the line is the question
-            temporary_storage += line + "\n" #To store the line in the temporary storage
+            if line.startswith("Question"): #To check if the line is the question
+                temporary_storage += line + "\n" #To store the line in the temporary storage
     
-        elif line.startswith("A:") or line.startswith("B:") or line.startswith("C:") or line.startswith("D:"): #To check if the line are the answers
-            temporary_storage += line + "\n" #To store the line in the temporary storage
+            elif line.startswith("A:") or line.startswith("B:") or line.startswith("C:") or line.startswith("D:"): #To check if the line are the answers
+                temporary_storage += line + "\n" #To store the line in the temporary storage
 
-        elif line.startswith("Right Answer"): #To check if the line is the correct answer in the current question
-            line = line.replace("Right Answer: ", "") #Removes the "Right Answer: " to make the answer remain 
-            self.right_answer_list.append(line) #To store the answer in the list
-            self.question_list.append(temporary_storage) #To store the question in the list
-            temporary_storage = "" #To reset the temporary
+            elif line.startswith("Right Answer"): #To check if the line is the correct answer in the current question
+                line = line.replace("Right Answer: ", "") #Removes the "Right Answer: " to make the answer remain 
+                self.right_answer_list.append(line) #To store the answer in the list
+                self.question_list.append(temporary_storage) #To store the question in the list
+                temporary_storage = "" #To reset the temporary
 
         else:
             print("Quiz file not found")
@@ -55,13 +55,43 @@ class QuizReader: #Imported the get quiz file for it probably works the same in 
 class QuizGame:
     def __init__(self):
         self.user_score = 0
-    def introduction():
+
+    def introduction(self):
         print("Welcome to the Quiz!") #To welcome the user
-        user_countinue = ("Would you like to take the quiz? Yes/No: ").lower() #To see if the user would countinue with the quiz
-        if user_countinue == "Yes":
+        user_countinue = input("Would you like to take the quiz? Yes/No: ").lower() #To see if the user would countinue with the quiz
+        if user_countinue == "yes":
             print("Let's go to the quiz!")
 
         else:
             print("Exiting program...") #If the user inputted no to exit the program
-            break
+            return
 
+    def quiz_start(self, question_list, right_answer_list):
+        random.shuffle(question_list, right_answer_list) #To shuffle the question list and right answer list to make it completely randomized
+
+        for question_number in enumerate(question_list): #Using enumerate to get the question number and get the question and correct answer from quiz_data
+            print(f"Question no.{question_number + 1}:\n {question_list}") #To prinnt the question
+            user_answer = input("Please input your answer(Ex: A): ").lower() #using lower() to ensure every user input lines up with right answer
+
+            if user_answer == right_answer_list[question_number]: #To line up to the question number
+                print("Correct!")
+                self.user_score += 1 #To add 1 point to the score
+            
+            else:
+                print("Wrong!")
+
+    def user_score_counter(self, question_list): #To see if the user passed or not
+
+        if self.user_score == len(question_list): #To see if user did a perfect job
+            print("You got a perfect score!", self.user_score,  "/",len(question_list)) 
+
+        elif self.user_score >= len(question_list): #To see if user passed
+            print("You passed!", self.user_score,  "/",len(question_list))
+
+        else: #The user failed
+            print("You failed, unfortunately", self.user_score,  "/",len(question_list))
+            
+reader = QuizReader()
+reader.get_quiz_file()
+game = QuizGame()
+game.quiz_start(reader.question_list, reader.right_answer_list)
